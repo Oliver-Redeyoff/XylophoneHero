27fdbf85e2fbe3528aa5226d692c59b34654076c
Will Cruse <willcruse123@protonmail.com>
2020-09-12 03:46:57 +0100

    feat: :sparkles: Add Instruments calculation code

---
+++ frontend/src/Instruments.js
@@ -0,0 +1,33 @@
+const instrumentTemplate = {
+    "xylophone": {
+        numBoxes: 7,
+        genBoxes: regularRectangleBoxes,
+    }
+};
+
+function instruments(name, minX, maxX, minY, maxY) {
+    const template = Object.create(instrumentTemplate[name]);
+    template.boxes = template.genBoxes(template.numBoxes, minX, maxX, minY, maxY, sounds[name])
+    delete template.genBoxes;
+    return template
+}
+
+function regularRectangleBoxes(numBoxes, minX, maxX, minY, maxY, sounds) {
+    const differenceX = maxX - minX;
+    const width = differenceX / numBoxes;
+    const boxes = [];
+    for (let i = 0; i < numBoxes; i++) {
+        const newMinX = minX + (width*i);
+        const newMaxX = newMinX + width;
+        const sound = sounds[i];
+        boxes.push({
+            minY: minY,
+            maxY: maxY,
+            minX: newMinX,
+            maxX: newMaxX,
+            sound: sound
+        });
+    }
+};
+
+export default instruments;
