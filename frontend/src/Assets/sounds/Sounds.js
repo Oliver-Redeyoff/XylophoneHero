f36273558e430175ab9a564c5d0208f2a45c4eb0
Will Cruse <willcruse123@protonmail.com>
2020-09-12 03:58:11 +0100

    feat: :sparkles: Add xylophone sounds to sounds js
    Moved xylophone sounds into frontend and created sounds.js with the sounds as part of the object

---
+++ frontend/src/Assets/sounds/Sounds.js
@@ -0,0 +1,21 @@
+import xylophoneA from './xylophone-sounds/a.mp3';
+import xylophoneB from './xylophone-sounds/b.mp3';
+import xylophoneC from './xylophone-sounds/c.mp3';
+import xylophoneD from './xylophone-sounds/d1.mp3';
+import xylophoneE from './xylophone-sounds/e1.mp3';
+import xylophoneF from './xylophone-sounds/f.mp3';
+import xylophoneG from './xylophone-sounds/g.mp3';
+
+const sounds = {
+    "xylophone": [
+        xylophoneA,
+        xylophoneB,
+        xylophoneC,
+        xylophoneD,
+        xylophoneE,
+        xylophoneF,
+        xylophoneG
+    ]
+}
+
+export default sounds;
