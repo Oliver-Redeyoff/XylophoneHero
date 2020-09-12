04fb0c512552a2aa4395125f02ea5caf7b33bdbb
Will <willcruse123@gmail.com>
2020-09-12 17:41:11 +0100

    Updates

---
+++ precache-manifest.b993e106b7a9e6e5e0870fe5e7a22ca2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f07539ccef430707df8dce4d551564",
+    "url": "/xylophoneHero/index.html"
+  },
+  {
+    "revision": "fd655d067e291183a01b",
+    "url": "/xylophoneHero/static/css/main.d552c7ab.chunk.css"
+  },
+  {
+    "revision": "155ddb896509949ba62a",
+    "url": "/xylophoneHero/static/js/2.199ba7d7.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/xylophoneHero/static/js/2.199ba7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd655d067e291183a01b",
+    "url": "/xylophoneHero/static/js/main.09b054fa.chunk.js"
+  },
+  {
+    "revision": "0d8e63aa945aaf517d2f",
+    "url": "/xylophoneHero/static/js/runtime-main.f44afea9.js"
+  },
+  {
+    "revision": "17dfe8a04d231814239acee21b656a6e",
+    "url": "/xylophoneHero/static/media/a.17dfe8a0.mp3"
+  },
+  {
+    "revision": "8e904fb2f64bb42ebf7f14b90d34e756",
+    "url": "/xylophoneHero/static/media/b.8e904fb2.mp3"
+  },
+  {
+    "revision": "1b390bd93db120ab384e83732c98ca5a",
+    "url": "/xylophoneHero/static/media/c.1b390bd9.mp3"
+  },
+  {
+    "revision": "54811c531ba0bac74f2ccc3a26eeda3c",
+    "url": "/xylophoneHero/static/media/d1.54811c53.mp3"
+  },
+  {
+    "revision": "73ef2d3437305e5ef8f45e3d698ffed8",
+    "url": "/xylophoneHero/static/media/e1.73ef2d34.mp3"
+  },
+  {
+    "revision": "925726cd5ef5919f34b1a6498f621872",
+    "url": "/xylophoneHero/static/media/f.925726cd.mp3"
+  },
+  {
+    "revision": "2fa9bbb6701404dea35ee5a0f195df94",
+    "url": "/xylophoneHero/static/media/g.2fa9bbb6.mp3"
+  },
+  {
+    "revision": "21cb9c8cb7e02098f95eefc7320b1edd",
+    "url": "/xylophoneHero/static/media/icon.21cb9c8c.png"
+  },
+  {
+    "revision": "8d63b82ebced1a04a66b3f0584c08ce5",
+    "url": "/xylophoneHero/static/media/settingsIcon.8d63b82e.png"
+  }
+]);
